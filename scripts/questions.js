d9844e9a07db4fd2550dc8c83dbd1b46bb0ce500
wayne mulovhedzi <wayne.mulovhedzi@intelliscient.co.za>
2023-07-17 21:26:18 +0200

    init

---
+++ scripts/questions.js
@@ -0,0 +1,114 @@
+const m1c1t3= [
+{
+question: "What is the purpose of a constructor in Java?",
+answers: [
+"1. To create an object of a class",
+"2. To initialize the instance variables of a class",
+"3. To define the behavior of a class",
+"4. To declare a variable in a class"
+],
+correct: [1],
+type: "single",
+uid: 0,
+img: false
+},
+{
+question: "Which keyword is used to create an object in Java?",
+answers: ["1. object", "2. new", "3. create", "4. instance"],
+correct: [1],
+type: "single",
+uid: 1,
+img: false
+},
+{
+question: "What is the output of the following code?\n\njava\nint x = 5;\nSystem.out.println(x++);\n",
+answers: ["1. 5", "2. 6", "3. 4", "4. Error"],
+correct: [0],
+type: "single",
+uid: 2,
+img: false
+},
+{
+question: "What is the correct way to declare a variable in Java?",
+answers: [
+"1. var x = 5;",
+"2. int x = 5;",
+"3. variable x = 5;",
+"4. int = 5;"
+],
+correct: [1],
+type: "single",
+uid: 3,
+img: false
+},
+{
+question: "Which data type is used to store a single character in Java?",
+answers: ["1. char", "2. string", "3. character", "4. letter"],
+correct: [0],
+type: "single",
+uid: 4,
+img: false
+},
+{
+question: "What is the output of the following code?\n\njava\nString str = \"Hello, World!\";\nSystem.out.println(str.length());\n",
+answers: ["1. 12", "2. 13", "3. 11", "4. Error"],
+correct: [2],
+type: "single",
+uid: 5,
+img: false
+},
+{
+question: "Which keyword is used to define a method in Java?",
+answers: ["1. function", "2. method", "3. define", "4. void"],
+correct: [1],
+type: "single",
+uid: 6,
+img: false
+},
+{
+question: "What is the purpose of the 'public' access modifier in Java?",
+answers: [
+"1. It allows access to the method or variable from anywhere",
+"2. It restricts access to the method or variable within the same class",
+"3. It allows access to the method or variable within the same package",
+"4. It allows access to the method or variable within subclasses"
+],
+correct: [0],
+type: "single",
+uid: 7,
+img: false
+},
+{
+question: "What is the output of the following code?\n\njava\nint[] numbers = {1, 2, 3, 4, 5};\nSystem.out.println(numbers[2]);\n",
+answers: ["1. 1", "2. 2", "3. 3", "4. 4"],
+correct: [2],
+type: "single",
+uid: 8,
+img: false
+},
+{
+question: "What is the correct way to declare a method that does not return any value in Java?",
+answers: [
+"1. void methodName()",
+"2. int methodName()",
+"3. methodName()",
+"4. String methodName()"
+],
+correct: [0],
+type: "single",
+uid: 9,
+img: false
+},
+{
+question: "What is the output of the following code?\n\njava\nint x = 10;\nif (x > 5) {\n System.out.println(\"Hello\");\n} else {\n System.out.println(\"World\");\n}\n",
+answers: ["1. Hello", "2. World", "3. Hello World", "4. No output"],
+correct: [0],
+type: "single",
+uid: 10,
+img: false
+}
+]
+
+
+
+
